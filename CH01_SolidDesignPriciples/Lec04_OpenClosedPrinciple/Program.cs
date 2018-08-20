3a694cfce24fe5dd6225f28151370221e255fe26
박필홍 <feelongpark>
2018-08-20 09:02:24 +0900

    lec 4 1:40

---
+++ CH01_SolidDesignPriciples/Lec04_OpenClosedPrinciple/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lec04_OpenClosedPrinciple
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    }
+
+    public class Produce
+    {
+        public string  Name;
+        public Color Color;
+        public Size Size;
+
+        public Produce(string name, Color color, Size size)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
