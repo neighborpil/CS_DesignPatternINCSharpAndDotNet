282a5d5b396d3ddc255eee0c3e55fba40fdcf1a5
박필홍 <feelongpark>
2018-08-22 09:29:13 +0900

    lec 04 finished

--- CH01_SolidDesignPriciples/Lec04_OpenClosedPrinciple/Program.cs
+++ CH01_SolidDesignPriciples/Lec04_OpenClosedPrinciple/Program.cs
@@ -17,13 +17,13 @@ namespace Lec04_OpenClosedPrinciple
         Small, Medium, Large, Huge
     }
 
-    public class Produce
+    public class Product
     {
         public string  Name;
         public Color Color;
         public Size Size;
 
-        public Produce(string name, Color color, Size size)
+        public Product(string name, Color color, Size size)
         {
             if(name == null)
             {
@@ -35,10 +35,136 @@ namespace Lec04_OpenClosedPrinciple
         }
     }
 
+    /// <summary>
+    /// old way
+    /// </summary>
+    public class ProductFilter
+    {
+        // 각각의 항목에 대하여 filter를 만들어 주어야 한다 - OpenClosedPrinciple을 만족하지 못한다
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        }
+
+        // OpenClosedPrinciple이란 extension에는 열려 있고, 수정에는 닫혀있어야 한다는 뜻 => filter를 수정하지 말아야 한다는 뜻
+    }
+
+    /// <summary>
+    /// 옵션 적는 부분
+    /// </summary>
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        Size size;
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            WriteLine("Green products (old)");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            var bf = new BetterFilter();
+            WriteLine("Green products (new)");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+                new AndSpecification<Product>(new ColorSpecification(Color.Blue),new SizeSpecification(Size.Large))))
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+
+            ReadKey();
         }
     }
 }
