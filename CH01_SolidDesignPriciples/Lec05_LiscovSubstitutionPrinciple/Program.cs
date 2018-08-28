5f53c46b66e8e0a31cc0d4d61de32f78deaccbed
neighborpil <neighborpil@naver.com>
2018-08-29 08:18:06 +0900

    lec5

---
+++ CH01_SolidDesignPriciples/Lec05_LiscovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec05_LiscovSubstitutionPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    /*
+    Liscov Substitution Principle : 상속하면  override로 지정하라??
+    */
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
+    class Program
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Square sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+
+            Rectangle sq2 = new Square(); // upcast가 일어나 결과가 안나온다
+            sq2.Width = 4;
+            Console.WriteLine($"{sq2} has area {Area(sq2)}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CH01_SolidDesignPriciples/LiscovSubstitutionPrincipleTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiscovSubstitutionPrincipleTest
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
+    class Program
+    {
+        public static int Area(Rectangle rect) => rect.Width * rect.Height;
+        static void Main(string[] args)
+        {
+            Rectangle sq = new Square();
+            sq.Width = 5;
+            Console.WriteLine($"{sq} has  area {Area(sq)}");
+            Console.ReadKey();
+        }
+    }
+}
