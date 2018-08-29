191758615b3eb26a2eada110aa79329bb764708b
박필홍 <feelongpark>
2018-08-29 09:12:41 +0900

    lec 10 finished

---
+++ CH01_SolidDesignPriciples/Lec06_InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec06_InterfaceSegregationPrinciple
+{
+    /*
+    # 하나의 큰 인터페이스를 만들기 보다는 작은 인터페이스들로 쪼개야 한다
+    */
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultifunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFasionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IScanner, IPrinter
+    {
+
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+                throw new ArgumentNullException(paramName: nameof(printer));
+            if (scanner == null)
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+            // decorator
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CH01_SolidDesignPriciples/Lec07_DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec07_DependencyInversionPrinciple
+{
+    /*
+    High-level 모듈은 Low-level모듈에 의존적이면 안된다
+
+    */
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+        // public DateTime DateOfBirth;;
+        // 생략
+    }
+
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+
+    // low-level
+    public class Relationships
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public List<(Person, Relationship, Person)> Relations => relations; // data structure가 직접 노출이되어 바꿀 수 없다
+    }
+
+    // high-level
+    public class RelationshipsHigh : IRelationshipBrowser
+    {
+
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public IEnumerable<Person> FindAllChildrenOf(string name) // 자료구조가 직접 노출되지 않아 자료구조를 바꿀 수 있다
+        {
+            //foreach (var r in relations.Where(
+            //    x => x.Item1.Name == "John" &&
+            //    x.Item2 == Relationship.Parent))
+            //{
+            //    yield return r.Item3;
+            //}
+            // 위와 동일
+            return relations.Where(x => x.Item3.Name == name && x.Item2 == Relationship.Parent)
+                            .Select(r => r.Item3);
+        }
+
+        public List<(Person, Relationship, Person)> Relations => relations;
+    }
+
+    class Research
+    {
+        // low-level
+        public Research(Relationships relationships)
+        {
+            var relations = relationships.Relations;
+            foreach (var r in relations.Where(
+                x => x.Item1.Name == "John" &&
+                x.Item2 == Relationship.Parent))
+            {
+                Console.WriteLine($"John has a child called {r.Item3.Name}");
+            }
+        }
+
+        // high-level
+        public Research(IRelationshipBrowser browser)
+        {
+            foreach (var p in browser.FindAllChildrenOf("John"))
+            {
+                Console.WriteLine($"John has a child called {p.Name}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Mary" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
+        }
+    }
+}
