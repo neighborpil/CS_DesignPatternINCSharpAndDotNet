e178671f1059dd218c56d37e177d4af04f689974
박필홍 <feelongpark>
2018-09-04 09:27:22 +0900

    lec 14 finished

---
+++ CH02_Builder/Lec14_FacetedBuilder/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec14_FacetedBuilder
+{
+    /*
+    # 하나의 object에 여러개의 Builder가 필요할 때 사용하는 법
+     */
+    
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Position;
+        public int AnnualIncom;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncom)}: {AnnualIncom}";
+        }
+    }
+
+    public class PersonBuilder // facade
+    {
+        // reference object!
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string street)
+        {
+            person.StreetAddress = street;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostCode(string postCode)
+        {
+            person.Postcode = postCode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string positon)
+        {
+            person.Position = positon;
+            return this;
+        }
+
+        public PersonBuilder Earning(int amount)
+        {
+            person.AnnualIncom = amount;
+            return this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives.At("123 Younho")
+                      .In("Busan")
+                      .WithPostCode("123")
+                .Works.At("Busan")
+                      .At("Engineer")
+                      .Earning(123000);
+
+            Console.WriteLine(person);
+        }
+    }
+}
--- CH02_Builder/Test_FluentBuilderInheritanceWithRecursiveGenerics/Program.cs
+++ CH02_Builder/Test_FluentBuilderInheritanceWithRecursiveGenerics/Program.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lec13_FluentBuilderInheritanceWithRecursiveGenerics
+namespace Test_FluentBuilderInheritanceWithRecursiveGenerics
 {
     public class Person
     {
         public string Name;
         public string Position;
 
-        public class Builder : PersonInfoBuilder<Builder>
+        public class Builder : PersonJobBuilder<Builder>
         {
-
         }
 
         public static Builder New => new Builder();
@@ -27,16 +26,16 @@ namespace Lec13_FluentBuilderInheritanceWithRecursiveGenerics
     public abstract class PersonBuilder
     {
         protected Person person = new Person();
+
         public Person Build()
         {
             return person;
         }
     }
-         
-    // 
+
     public class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
     {
-        //protected Person person = new Person(); // 상속 사용
+
         public SELF Called(string name)
         {
             person.Name = name;
@@ -44,9 +43,9 @@ namespace Lec13_FluentBuilderInheritanceWithRecursiveGenerics
         }
     }
 
-    public class PersonJobBuilder<SELF>: PersonInfoBuilder<PersonJobBuilder<SELF>> where SELF : PersonJobBuilder<SELF>
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<SELF> where SELF : PersonJobBuilder<SELF>
     {
-        public SELF WorksAsA(string position)
+        public SELF WorkAsA(string position)
         {
             person.Position = position;
             return (SELF)this;
@@ -57,19 +56,15 @@ namespace Lec13_FluentBuilderInheritanceWithRecursiveGenerics
     {
         static void Main(string[] args)
         {
-            //var builder = new PersonJobBuilder();
-            //builder.Called("dmitri")
-            //       .WorksAsAs // 오류가 뜬다 왜냐하면 Called메서드는 PersonInfoBuilder를 Return하기 때문이다
+            //var me = new PersonJobBuilder()
+            //    .Called("feel")
+            //    .wa
 
             var me = Person.New
                 .Called("feel")
-                .WorksAsA("programmer")
+                .WorkAsA("Programmer")
                 .Build();
             Console.WriteLine(me);
-
-            var personJobBuilder = Person.New
-                .Called("feel");
-            Console.WriteLine(personJobBuilder);
         }
     }
 }
