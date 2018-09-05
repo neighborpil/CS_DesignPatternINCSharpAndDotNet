eac09894b18be0ecfa1688a26a5ada98b993fe80
박필홍 <feelongpark>
2018-09-06 08:42:55 +0900

    Lec21 abstract factory finished

---
+++ CH03_Factories/Test_AbstractFactory/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_AbstractFactory
+{
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Enjoy Tea!");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Enjoy Coffee");
+        }
+    }
+
+    public interface IHotDrinkFacgory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFacgory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Boil water and pour {amount}ml, Enjoy!");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFacgory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Boil water and pour {amount}ml, Enjoy!");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink : byte
+        {
+            Tea,
+            Coffee
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFacgory> factories = new Dictionary<AvailableDrink, IHotDrinkFacgory>();
+
+        public HotDrinkMachine()
+        {
+            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var factory = (IHotDrinkFacgory)Activator.CreateInstance(
+                    Type.GetType($"Test_AbstractFactory.{Enum.GetName(typeof(AvailableDrink), drink)}Factory"));
+                factories.Add(drink, factory);
+            }
+        }
+
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        {
+            return factories[drink].Prepare(amount);
+        }
+	
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HotDrinkMachine machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 10);
+            drink.Consume();
+            drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 5);
+            drink.Consume();
+
+        }
+    }
+}
---
+++ CH03_Factories/Test_InnerFactory/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_InnerFactory
+{
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+        public static Point Origin => new Point(0, 0);
+        public static Point Orgin2 = new Point(0, 0); // better
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pt1 = Point.Factory.NewPolarPoint(1, 2);
+            Console.WriteLine(pt1);
+            var pt2 = Point.Factory.NewCartesianPoint(1, Math.PI / 4);
+            Console.WriteLine(pt2);
+
+        }
+    }
+}
