aa51ebce9eeba5b0728b7962785757d702842241
박필홍 <feelongpark>
2018-09-11 09:04:05 +0900

    lec25 making

--- CH04_Prototype/Lec25_IClonableIsBad/Program.cs
+++ CH04_Prototype/Lec25_IClonableIsBad/Program.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace Lec25_IClonableIsBad
 {
-    public class Person
+    public class Person : ICloneable
     {
         public string[] Names;
-        private Address Address;
+        public Address Address;
 
         public Person(string[] names, Address address)
         {
             this.Names = names ?? throw new ArgumentNullException(paramName: nameof(names));
             this.Address = address ?? throw new ArgumentNullException(paramName: nameof(address));
+        }
+
+        //shallow copy
+        public object Clone()
+        {
+            return new Person(Names, Address);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
         }
     }
 
@@ -23,12 +34,39 @@ namespace Lec25_IClonableIsBad
         public string StreetName;
         public int HouseNumber;
 
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(paramName: nameof(streetName));
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+
+            //var jane = john;
+            //jane.Names[0] = "Jane";
+            
+            //shallow copy
+            var jane = (Person)john.Clone();
+            jane.Address.HouseNumber = 321;
+
+            // deep copy
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+
+
+
+
         }
     }
 }
