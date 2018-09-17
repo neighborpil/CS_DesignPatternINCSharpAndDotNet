a18de0e91508c568688282c1160abe4b54c0755f
박필홍 <feelongpark>
2018-09-17 13:36:47 +0900

    brigde start

---
+++ CH06_Adapter/Test_AdapterCashing/Program.cs
@@ -0,0 +1,174 @@
+﻿using MoreLinq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using static System.Console;
+
+namespace Test_AdapterCashing
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(Point other)
+        {
+            return other.X == this.X && other.Y == this.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line(Point start, Point end)
+        {
+            Start = start ?? throw new ArgumentNullException(paramName: nameof(start));
+            End = end ?? throw new ArgumentNullException(paramName: nameof(end));
+        }
+
+        public bool Equals(Line other)
+        {
+            return Equals(Start, other.Start) && Equals(End, other.End);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (this.GetType() != obj.GetType()) return false;
+            return Equals((Line) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Start != null ? Start.GetHashCode() : 0) * 397) ^ (End != null ? End.GetHashCode() : 0);
+            }
+        }
+    }
+
+    public class VectorObject : Collection<Line>
+    {
+
+    }
+
+    public class VectorRectangle : VectorObject
+    {
+        public VectorRectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+
+    public class LineToPointAdapter : IEnumerable<Point>
+    {
+        public static int count;
+        static Dictionary<int, List<Point>> cache = new Dictionary<int, List<Point>>();
+        private int hash;
+
+        public LineToPointAdapter(Line line)
+        {
+            hash = line.GetHashCode();
+            if (cache.ContainsKey(hash)) return;
+
+            WriteLine($"{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}]");
+
+            var points = new List<Point>();
+
+            int left = Math.Min(line.Start.X, line.End.X);
+            int right = Math.Max(line.Start.X, line.End.X);
+            int top = Math.Min(line.Start.Y, line.End.Y);
+            int bottom = Math.Max(line.Start.Y, line.End.Y);
+            int dx = left - right;
+            int dy = top - bottom;
+
+            if(dx == 0)
+            {
+                for (int y = top; y <= bottom; y++)
+                {
+                    points.Add(new Point(left, y));
+                }
+            }
+
+            if(dy == 0)
+            {
+                for (int x = left; x <= right; x++)
+                {
+                    points.Add(new Point(x, top));
+                }
+            }
+
+            cache.Add(hash, points);
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            return cache[hash].GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        public static List<VectorObject> vectorObjects = new List<VectorObject>()
+        {
+            new VectorRectangle(1, 1, 10, 10),
+            new VectorRectangle(3, 3, 10, 10)
+        };
+
+        public static void DrawPoint(Point point)
+        {
+            Write($"({point.X},{point.Y})");
+        }
+
+        static void Main(string[] args)
+        {
+            Draw();
+            Draw();
+        }
+
+        private static void Draw()
+        {
+            foreach (var vo in vectorObjects)
+            {
+                foreach (var line in vo)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    adapter.ForEach(DrawPoint);
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
--- CH06_Adapter/Test_VectorRasterDemo/Program.cs
+++ CH06_Adapter/Test_VectorRasterDemo/Program.cs
@@ -2,12 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq.Expressions;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
 
-namespace Lec37_VectorRasterDemo
+namespace Test_VectorRasterDemo
 {
     public class Point
     {
@@ -33,14 +33,13 @@ namespace Lec37_VectorRasterDemo
 
     public class VectorObject : Collection<Line>
     {
-        
+
     }
 
     public class VectorRectangle : VectorObject
     {
         public VectorRectangle(int x, int y, int width, int height)
         {
-            //4개의 라인 추가
             Add(new Line(new Point(x, y), new Point(x + width, y)));
             Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
             Add(new Line(new Point(x, y), new Point(x, y + height)));
@@ -61,21 +60,20 @@ namespace Lec37_VectorRasterDemo
             int top = Math.Min(line.Start.Y, line.End.Y);
             int bottom = Math.Max(line.Start.Y, line.End.Y);
             int dx = right - left;
-            //int dy = line.End.Y - line.Start.Y;
             int dy = top - bottom;
 
-            if(dx == 0) // 세로선
+            if(dx == 0)
             {
                 for (int y = top; y <= bottom; y++)
                 {
                     Add(new Point(left, y));
                 }
             }
-            else if(dy == 0) // 가로선
+            else if(dy == 0)
             {
-                for (int x = left; x <= right; ++x)
+                for (int x = left; x <= right; x++)
                 {
-                    Add(new Point(x, top)); // top, bottom은 같기 때문에 어느것 써도 된다
+                    Add(new Point(x, top));
                 }
             }
         }
@@ -83,15 +81,15 @@ namespace Lec37_VectorRasterDemo
 
     class Program
     {
-        private static readonly List<VectorObject> vectorObjects = new List<VectorObject>
+        private static readonly List<VectorObject> vectorObject = new List<VectorObject>()
         {
             new VectorRectangle(1, 1, 10, 10),
             new VectorRectangle(3, 3, 6, 6)
         };
 
-        public static void DrawPoint(Point p)
+        public static void DrawPoint(Point point)
         {
-            Write($"({p.X}.{p.Y})");
+            Write($"({point.X},{point.Y})");
         }
 
         static void Main(string[] args)
@@ -101,13 +99,13 @@ namespace Lec37_VectorRasterDemo
 
         private static void Draw()
         {
-            foreach (var vo in vectorObjects)
+            foreach (var vo in vectorObject)
             {
                 foreach (var line in vo)
                 {
                     var adapter = new LineToPointAdapter(line);
                     adapter.ForEach(DrawPoint);
-                    Console.WriteLine();
+                    WriteLine();
                 }
             }
         }
