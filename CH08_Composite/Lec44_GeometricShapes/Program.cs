5d2faddeba4ed01720d07e29a0d2e28a4e89e440
박필홍 <feelongpark>
2018-09-18 22:04:42 +0900

    ch08 composite start

---
+++ CH08_Composite/Lec44_GeometricShapes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec44_GeometricShapes
+{
+    class Program
+    {
+        public class GraphicObject
+        {
+            public virtual string Name { get; set; } = "Group";
+            public string Color;
+
+            private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+            public List<GraphicObject> Children => children.Value;
+
+            private void Print(StringBuilder sb, int depth)
+            {
+                sb.Append(new string('*', depth))
+                    .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+                    .AppendLine(Name);
+                foreach (var item in collection)
+                {
+
+                }
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                Print(sb, 0);
+                return sb.ToString();
+            }
+        }
+
+        public class Circle : GraphicObject
+        {
+            public override string Name => "Circle";
+        }
+
+        public class Square : GraphicObject
+        {
+            public override string Name => "Square";
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
