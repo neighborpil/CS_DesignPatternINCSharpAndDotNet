a74d2052461f67cf5e415b13bed0cc4effce5106
박필홍 <feelongpark>
2018-09-21 09:03:46 +0900

    lec staticdecorationcomposition finished

---
+++ CH09_Decorator/Lec51_DynamicDecoratorComposition/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec51_DynamicDecoratorComposition
+{
+    public interface IShape
+    {
+        string AsString();
+    }
+
+    public class Circle : IShape
+    {
+        private float radius;
+
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
+        
+        public void Resize(float factor)
+        {
+            radius *= factor;
+        }
+
+        public string AsString() => $"A circle with radius {radius}";
+    }
+
+    public class Square : IShape
+    {
+        private float side;
+
+        public Square(float side)
+        {
+            this.side = side;
+        }
+
+        public string AsString() => $"A square with side {side}";
+    }
+
+    public class ColoredShape : IShape
+    {
+        private IShape shape;
+        private string color;
+
+        public ColoredShape(IShape shape, string color)
+        {
+            this.shape = shape ?? throw new ArgumentNullException(paramName: nameof(shape));
+            this.color = color ?? throw new ArgumentNullException(paramName: nameof(color));
+        }
+
+        public string AsString() => $"{shape.AsString()} has the color {color}";
+    }
+
+    public class TransparentShape : IShape
+    {
+        private IShape shape;
+        private float transparency;
+
+        public TransparentShape(IShape shape, float transparency)
+        {
+            this.shape = shape ?? throw new ArgumentNullException(paramName: nameof(shape));
+            this.transparency = transparency;
+        }
+
+        public string AsString() => $"{shape.AsString()} has {transparency * 100.0}% transparency";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(1.23f);
+            Console.WriteLine(square.AsString());
+
+            ColoredShape redSquare = new ColoredShape(square, "Red");
+            Console.WriteLine(redSquare.AsString());
+
+            TransparentShape redHalfTransparentSquare = new TransparentShape(redSquare, 0.5f);
+            Console.WriteLine(redHalfTransparentSquare.AsString());
+        }
+    }
+}
