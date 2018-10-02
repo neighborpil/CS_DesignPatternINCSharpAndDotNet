0f50b37a31ae862910cabc8e2ef7ac5cfd86954e
박필홍 <feelongpark>
2018-10-03 08:13:00 +0900

    to test dotmemoryunit, I need to install reshaprer. But computer in my office does'nt have it, so I'll do it later in my home

---
+++ CH10_Facade/Lec55_Facade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec55_Facade
+{
+    /*
+    #facade pattern
+     - 퍼사드 패턴은 인터페이스를 단순화시키기 위해서 인터페이스를 변경한다.
+       때문에 훨씬 쓰기 쉬운 인터페이스를 제공하는 퍼사드 클래스를 구현함으로써 복잡한 시스템을 훨씬 쉽게 사용할 수 있다.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
