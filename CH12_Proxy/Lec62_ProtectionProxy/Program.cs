0d9dcdd45fc00e089c58d9f9c3d6bd37c787895f
박필홍 <feelongpark>
2018-10-04 08:57:18 +0900

    lec62 finished

---
+++ CH12_Proxy/Lec62_ProtectionProxy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec62_ProtectionProxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is being driven");
+        }
+    }
+
+    public class Driver
+    {
+        public Driver(int age)
+        {
+            Age = age;
+        }
+
+        public int Age { get; set; }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Driver driver;
+        private Car car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+            {
+                Console.WriteLine("too young");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(122));
+            car.Drive();
+        }
+    }
+}
