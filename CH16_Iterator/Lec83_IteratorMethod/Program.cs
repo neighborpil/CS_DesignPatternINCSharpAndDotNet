9909aaaaa3f8bceea69b3ceb75530c36aff7d929
박필홍 <feelongpark>
2019-01-16 13:15:06 +0900

    lec83 iteratormethod finished

---
+++ CH16_Iterator/Lec83_IteratorMethod/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec83_IteratorMethod
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+            left.Parent = right.Parent = this;
+        }
+    }
+
+    public class BinaryTree<T>
+    {
+        private Node<T> root;
+
+        public BinaryTree(Node<T> root)
+        {
+            this.root = root;
+        }
+
+        public IEnumerable<Node<T>> InOrder
+        {
+            get
+            {
+                IEnumerable<Node<T>> Traverse(Node<T> current)
+                {
+                    if (current.Left != null)
+                        foreach (var left in Traverse(current.Left))
+                            yield return left;
+                    yield return current;
+                    if(current.Right != null)
+                        foreach (var right in Traverse(current.Right))
+                            yield return right;
+                }
+                foreach (var node in Traverse(root))
+                    yield return node;
+            }
+        }
+    }
+
+    public class InOrderIterator<T>
+    {
+        private readonly Node<T> root;
+        public Node<T> Current;
+        private bool yieldedStart;
+
+        public InOrderIterator(Node<T> root)
+        {
+            this.root = Current = root;
+            while (Current.Left != null)
+                Current = Current.Left;
+        }
+
+        public bool MoveNext()
+        {
+            if (!yieldedStart)
+            {
+                yieldedStart = true;
+                return true;
+            }
+            if (Current.Right != null)
+            {
+                Current = Current.Right;
+                while (Current.Left != null)
+                    Current = Current.Left;
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while (p != null && Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+                Current = p;
+                return Current != null;
+            }
+        }
+
+        public void Reset()
+        {
+            Current = root;
+            yieldedStart = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+            // C++ style
+            var it = new InOrderIterator<int>(root);
+            while (it.MoveNext())
+            {
+                Console.Write(it.Current.Value);
+                Console.Write(",");
+            }
+            Console.WriteLine();
+            // C# style
+            var tree = new BinaryTree<int>(root);
+            Console.WriteLine(string.Join(",", 
+                tree.InOrder.Select(x => x.Value)));
+
+            Console.ReadKey();
+        }
+    }
+}
