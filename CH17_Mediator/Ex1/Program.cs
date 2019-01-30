8463a4105de7aeafe409e45386d136575827822d
neighborpil <neighborpil@naver.com>
2019-01-31 00:08:27 +0900

    mediator finished

---
+++ CH17_Mediator/Ex1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static  System.Console;
+namespace Ex1
+{
+    public class Participant
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        private Mediator mediator;
+
+        public Participant(Mediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(paramName: nameof(mediator));
+            mediator.Join(this);
+        }
+
+        public Participant(string name, Mediator mediator) : this(mediator)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+        }
+
+        public void Say(int value)
+        {
+            mediator.Broadcast(Name, value);
+            WriteLine($"{Name} says {value}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Value}";
+        }
+    }
+
+    public class Mediator
+    {
+        List<Participant> participants = new List<Participant>();
+
+        public void Join(Participant participant)
+        {
+            this.participants.Add(participant);
+        }
+
+        public void Broadcast(string name, int value)
+        {
+            participants.ForEach(p =>
+            {
+                if (!p.Name.Equals(name))
+                    p.Value += value;
+            });
+        }
+
+        public void Saying()
+        {
+            participants.ForEach(p => WriteLine(p));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator m = new Mediator();
+            Participant p1 = new Participant("john", m);
+            Participant p2 = new Participant("chris", m);
+            m.Saying();
+            p1.Say(3);
+            m.Saying();
+            p2.Say(1);
+            m.Saying();
+            ReadKey();
+        }
+    }
+}
+
+namespace Ex1.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestSuite()
+        {
+            Mediator m = new Mediator();
+            Participant p1 = new Participant("john", m);
+            Participant p2 = new Participant("chris", m);
+            Assert.That(p1.Value, Is.EqualTo(0));
+            Assert.That(p2.Value, Is.EqualTo(0));
+            p1.Say(3);
+            Assert.That(p1.Value, Is.EqualTo(0));
+            Assert.That(p2.Value, Is.EqualTo(3));
+            p2.Say(1);
+            Assert.That(p1.Value, Is.EqualTo(1));
+            Assert.That(p2.Value, Is.EqualTo(3));
+        }
+    }
+
+}
---
+++ CH17_Mediator/Ex1_Answer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Ex1_Answer
+{
+    public class Participant
+    {
+        private readonly Mediator mediator;
+        public int Value { get; set; }
+
+        public Participant(Mediator mediator)
+        {
+            this.mediator = mediator;
+            mediator.Alert += Mediator_Alert;
+        }
+
+        private void Mediator_Alert(object sender, int e)
+        {
+            if (sender != this)
+                Value += e;
+        }
+
+        public void Say(int n)
+        {
+            mediator.Broadcast(this, n);
+        }
+    }
+
+    public class Mediator
+    {
+        public event EventHandler<int> Alert;
+
+        public void Broadcast(object sender, int n)
+        {
+            Alert?.Invoke(sender, n);
+        }
+    }
+
+}
+
+
+namespace Ex1_Answer.Tests
+{
+    [TestFixture]
+    public class TestSuite
+    {
+        [Test]
+        public void Test()
+        {
+            Mediator mediator = new Mediator();
+            var p1 = new Participant(mediator);
+            var p2 = new Participant(mediator);
+
+            Assert.That(p1.Value, Is.EqualTo(0));
+            Assert.That(p2.Value, Is.EqualTo(0));
+
+            p1.Say(2);
+
+            Assert.That(p1.Value, Is.EqualTo(0));
+            Assert.That(p2.Value, Is.EqualTo(2));
+
+            p2.Say(4);
+
+            Assert.That(p1.Value, Is.EqualTo(4));
+            Assert.That(p2.Value, Is.EqualTo(2));
+        }
+    }
+}
