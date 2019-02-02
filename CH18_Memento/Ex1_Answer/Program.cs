2abd7c958fccfba76bcb95810809251e13bab8ee
neighborpil <neighborpil@naver.com>
2019-02-02 10:39:56 +0900

    null object

---
+++ CH18_Memento/Ex1_Answer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Ex1_Answer
+{
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        public List<Token> Tokens = new List<Token>();
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            return AddToken(new Token(value));
+        }
+
+        public Memento AddToken(Token token)
+        {
+            Tokens.Add(token);
+            var m = new Memento();
+            m.Tokens = Tokens.Select(t => new Token(t.Value)).ToList();
+            return m;
+        }
+
+        public void Revert(Memento m)
+        {
+            Tokens = m.Tokens.Select(mm => new Token(mm.Value)).ToList();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+namespace Ex1_Answer.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SingleTokenTest()
+        {       
+            var tm = new TokenMachine();
+            var m = tm.AddToken(123);
+            tm.AddToken(456);
+            tm.Revert(m);
+            Assert.That(tm.Tokens.Count, Is.EqualTo(1));
+            Assert.That(tm.Tokens[0].Value, Is.EqualTo(123));
+        }
+    }
+}
---
+++ CH19_NullObject/Lec97_NullObject/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using JetBrains.Annotations;
+using static System.Console;
+namespace Lec97_NullObject
+{
+    interface ILog
+    {
+        void Info(string msg);
+        void Warn(string msg);
+    }
+
+    class ConsoleLog : ILog
+    {
+        public void Info(string msg)
+        {
+            WriteLine(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            WriteLine($"WARNING!!{msg}");
+        }
+    }
+
+    class BankAccount
+    {
+        private ILog log;
+        private int balance;
+
+        public BankAccount(/*[CanBeNull]*/ ILog log)
+        {
+            this.log = log;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            log/*?*/.Info($"Deposited {amount}, balance is now {balance}");
+        }
+    }
+
+    public class NullLog : ILog
+    {
+        public void Info(string msg)
+        {
+        }
+
+        // 만약 반환값이 있다면 default 값을 반환한다
+        //public int Info(string msg)
+        //{
+        //  return default(int);
+        //}
+
+        public void Warn(string msg)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var log = new ConsoleLog();
+
+            //var ba = new BankAccount(null); // null일 경우 오류가 난다
+            //ba.Deposit(100);
+
+            var cb = new ContainerBuilder();
+            //cb.RegisterType<BankAccount>();
+            //cb.RegisterInstance((ILog) null); // null을 등록 할 수 없다
+            //cb.Register(ctx => new BankAccount(null)); // 등록 가능
+            cb.RegisterType<BankAccount>();
+            cb.RegisterType<NullLog>().As<ILog>();
+
+            using (var c = cb.Build())
+            {
+                var ba = c.Resolve<BankAccount>();
+
+            }
+
+        }
+    }
+}
